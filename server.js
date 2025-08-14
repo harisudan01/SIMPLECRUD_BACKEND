d15533140dea7f2af4869f15f6758c25564af326
CEN-E321-23\Centizen_User <harisudan@centizen.com>
2025-08-14 16:17:47 +0530

    Initial commit for Node backend

--- server.js
+++ server.js
@@ -6,6 +6,12 @@ const app = express();
 app.use(cors());
 app.use(express.json());
 app.use('/v1/product', v1);
+const cors = require('cors');
+app.use(cors({
+  origin: 'https://simplecrud-frontend.vercel.app',
+  methods: ['GET', 'POST', 'PUT', 'DELETE'],
+  credentials: true
+}));
 // Start server
 sequelize.sync({ alter: true }).then(() => {
   app.listen(5000, () => console.log("Server running on port 5000"));
